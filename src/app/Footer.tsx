0d9cd88c84e6b5a07865c38e726dfe2d8553f07c
Shahzaib Awan <awan.shahzaib.0314@gmail.com>
2025-08-23 00:59:29 +0500

    Updated project

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -4,8 +4,8 @@ import { FaGoogle, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return ( 
-    <footer className="bg-[#0F241D] text-white pb-10 px-30 font-medium p-15 text-xl">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-">
+    <footer className="bg-[#0F241D] text-white pb-10 px-30 font-medium p-15 text-1xl">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 ">
         {/* Logo & About */}
         <div className="mt-10">
           <h2 className="text-lg font-medium font-open ">Logo</h2>
